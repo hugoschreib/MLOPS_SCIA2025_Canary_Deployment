8bc0bb554611faa470accb530589d00966659c38
Hugo Schreiber <hugo.schreiber@epita.fr>
2025-09-15 16:56:05 +0200

    feat: canary

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import mlflow
 import pandas as pd
-
+import random
 
 class MyModel(mlflow.pyfunc.PythonModel):
     def predict(self, context, model_input, params=None):
@@ -24,11 +24,16 @@ class Body(BaseModel):
     petal_length: float
     petal_width: float
 
+CURRENT_MODEL = 1
+NEXT_MODEL = 2
+P = 0.2
+IS_ACCEPTED = False
 
 model = mlflow.pyfunc.load_model(f"models:/tracking-quickstart/2")
 
 @app.post("/predict")
 def predict(body: Body):
+    model = mlflow.pyfunc.load_model(f"models:/tracking-quickstart/{NEXT_MODEL if IS_ACCEPTED else (NEXT_MODEL if random.randint(0,100) < int(P * 100) else CURRENT_MODEL)}")
     x_new = pd.DataFrame([[body.sepal_length, body.sepal_width, body.petal_length, body.petal_width]])
     res = model.predict(x_new)[0]
     print(res)
@@ -39,9 +44,24 @@ class Model(BaseModel):
 
 @app.post("/update-model")
 def update(body: Model):
-    global model
+    global model, IS_ACCEPTED, CURRENT_MODEL, NEXT_MODEL
     try:
+        CURRENT_MODEL = body.version
+        NEXT_MODEL = body.version + 1
+        IS_ACCEPTED = 0
         model = mlflow.pyfunc.load_model(f"models:/tracking-quickstart/{body.version}")
         return {"status": "SUCCESS"}
     except:
         return {"status": "ERROR"}
+
+@app.post("/accept-next-model")
+def accept():
+    global IS_ACCEPTED, CURRENT_MODEL, NEXT_MODEL
+    IS_ACCEPTED = True
+    CURRENT_MODEL = NEXT_MODEL
+    return {"status": "SUCCESS"}
+
+
+@app.get("/version")
+def version():
+    return {"version": CURRENT_MODEL}
